04f47737aa3604ee8e01e1187e9118e313415bbb
AlephTet <mrmaxboyyt@gmail.com>
2025-01-22 23:46:52 -0500

    j

--- src/Client.java
+++ src/Client.java
@@ -1,26 +1,39 @@
 import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
-
-    private static final String SERVER_ADDRESS = "100.36.119.115";
+    private static final String SERVER_IP = "100.36.119.115";
     private static final int SERVER_PORT = 443;
 
     public static void main(String[] args) {
-        System.out.println("Connecting to the server...");
-
-        try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+        try (Socket socket = new Socket(SERVER_IP, SERVER_PORT);
              BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true)) {
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             Scanner scanner = new Scanner(System.in)) {
 
-            System.out.println("Connected to the server.");
-            String response;
-            while ((response = in.readLine()) != null) {
+            System.out.println("Connected to the server at " + SERVER_IP + ":" + SERVER_PORT);
+            System.out.println("Type commands to interact with the server. Type 'exit' to close the connection.");
+
+            while (true) {
+                System.out.print("Enter command: ");
+                String command = scanner.nextLine();
+                out.println(command);
+
+                String response = in.readLine();
+                if (response == null) {
+                    System.out.println("Server closed the connection.");
+                    break;
+                }
                 System.out.println("Server response: " + response);
-            }
 
-        } catch (Exception e) {
-            System.err.println("Error connecting to the server: " + e.getMessage());
+                if (command.equalsIgnoreCase("exit")) {
+                    System.out.println("Exiting the client.");
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Unable to connect to the server. Please check the IP address and port.");
             e.printStackTrace();
         }
     }
--- src/MainMenu.java
+++ src/MainMenu.java
@@ -15,11 +15,15 @@ import java.awt.event.ActionListener;
 import java.awt.geom.Area;
 import java.awt.geom.RoundRectangle2D;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
+import java.net.Socket;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.swing.ImageIcon;
@@ -33,47 +37,37 @@ import javax.swing.border.EmptyBorder;
 public class MainMenu extends JFrame 
 {
 	private static final Color bgColor = new Color(221,220,226,255);
+	private static final String SERVER_IP = "100.36.119.115";
+	private static final int SERVER_PORT = 443;
 	public MainMenu() 
 	{
 		super("JKLM");
-		String urlString = "https://jklm.fun/api/rooms";
-		StringBuilder response = null;
-        try 
-        {
-            URL url = new URL(urlString);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            int responseCode = connection.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK)
-            {
-                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                response = new StringBuilder();
-                String inputLine;
-
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                in.close();
-            } else {
-                System.out.println("GET request failed. Response Code: " + responseCode);
-            }
-            connection.disconnect();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        String rooms = response.toString();
-        Map<String, String> map = new HashMap<>();
-        rooms = rooms.substring(16);
-        while(rooms.length()>15)
-        {
-        	//System.out.println(rooms.substring(rooms.indexOf("gameId")+9,rooms.indexOf("gameId")+13));
-        	if(rooms.substring(rooms.indexOf("gameId")+9,rooms.indexOf("gameId")+13).equals("bomb"))
-        	{
-        		System.out.println("bomb "+rooms.substring(rooms.indexOf("roomCode")+11,rooms.indexOf("roomCode")+15));
-        		map.put(urlString, rooms.substring(rooms.indexOf("roomCode")+11,rooms.indexOf("roomCode")+15));
-        	}
-        	rooms = rooms.substring(rooms.indexOf("}")+1);
-        }
+		String response = "";
+		try (Socket socket = new Socket(SERVER_IP, SERVER_PORT);
+				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
+			String command = "fetch_rooms";
+			out.println(command);
+			response = in.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String rooms = response;
+		Map<String, String> map = new HashMap<>();
+		ArrayList<Integer> list = new ArrayList<>();
+		rooms = rooms.substring(16);
+		while(rooms.length()>15)
+		{
+			//System.out.println(rooms.substring(rooms.indexOf("gameId")+9,rooms.indexOf("gameId")+13));
+			if(rooms.substring(rooms.indexOf("gameId")+9,rooms.indexOf("gameId")+13).equals("bomb"))
+			{
+				System.out.println(rooms.substring(rooms.indexOf("name")+7,rooms.indexOf(",",rooms.indexOf("name")+7)-1)+" "+rooms.substring(rooms.indexOf("roomCode")+11,rooms.indexOf("roomCode")+15));
+				map.put(rooms.substring(rooms.indexOf("name")+7,rooms.indexOf(",",rooms.indexOf("name")+7)-1), rooms.substring(rooms.indexOf("roomCode")+11,rooms.indexOf("roomCode")+15));
+				list.add(Integer.parseInt(rooms.substring(rooms.indexOf("playerCount")+13,rooms.indexOf(",",rooms.indexOf("playerCount")+13))));
+				System.out.println(rooms.substring(rooms.indexOf("playerCount")+13,rooms.indexOf(",",rooms.indexOf("playerCount")+13)));
+			}
+			rooms = rooms.substring(rooms.indexOf("}")+1);
+		}
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(null);
 		getContentPane().setBackground(bgColor);
@@ -82,7 +76,7 @@ public class MainMenu extends JFrame
 		playE.setBackground(Color.GREEN);
 		playE.setBounds(500,500,100,50);
 		add(playE);
-		
+
 		JButton quit = new JButton("Quit Game");
 		quit.addActionListener(new ActionListener() 
 		{
